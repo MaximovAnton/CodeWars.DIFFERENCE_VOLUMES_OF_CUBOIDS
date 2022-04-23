fdded4f4c52f2a6eb95a5d46c777fc4d0e0dcf8f
anton <maximov.antonn@gmail.com>
2022-04-23 19:03:21 +0300

    dif

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ function findDifference(a, b){
     return Math.abs((a[0] * a[1] * a[2]) - (b[0] * b[1] * b[2]))
 }
 
-//document.write(findDifference([1,2,4], [1,2,3]))
+document.write(findDifference([1,2,4], [1,2,3]))
 document.write()
 
